rohan-agra/VOL-2738-voltha-lib-go

[VOL-2738]Enable configuration of ectd v3 client options

pkg/db/backend.go
@@ -82,7 +82,7 @@ func (b *Backend) newClient(address string, timeout int) (kvstore.Client, error)
 	case "consul":
 		return kvstore.NewConsulClient(address, timeout)
 	case "etcd":
-		return kvstore.NewEtcdClient(address, timeout)
+		return kvstore.NewEtcdClient(address, timeout, kvstore.DefaultLogLevel)
 	}
 	return nil, errors.New("unsupported-kv-store")
 }

pkg/ponresourcemanager/ponresourcemanager.go
@@ -161,7 +161,7 @@ func newKVClient(storeType string, address string, timeout int) (kvstore.Client,
 	case "consul":
 		return kvstore.NewConsulClient(address, timeout)
 	case "etcd":
-		return kvstore.NewEtcdClient(address, timeout)
+		return kvstore.NewEtcdClient(address, timeout, kvstore.DefaultLogLevel)
 	}
 	return nil, errors.New("unsupported-kv-store")
 }

pkg/techprofile/tech_profile.go
@@ -275,7 +275,7 @@ func newKVClient(storeType string, address string, timeout int) (kvstore.Client,
 	case "consul":
 		return kvstore.NewConsulClient(address, timeout)
 	case "etcd":
-		return kvstore.NewEtcdClient(address, timeout)
+		return kvstore.NewEtcdClient(address, timeout, kvstore.DefaultLogLevel)
 	}
 	return nil, errors.New("unsupported-kv-store")
 }

opencord/voltha-lib-go

Change-Id: I75541525b3fd929adf6325f743def45c77e8930a

pkg/db/backend.go
@@ -82,7 +82,7 @@ func (b *Backend) newClient(address string, timeout int) (kvstore.Client, error)
 	case "consul":
 		return kvstore.NewConsulClient(address, timeout)
 	case "etcd":
-		return kvstore.NewEtcdClient(address, timeout)
+		return kvstore.NewEtcdClient(address, timeout, log.WarnLevel)
 	}
 	return nil, errors.New("unsupported-kv-store")
 }

pkg/ponresourcemanager/ponresourcemanager.go
@@ -161,7 +161,7 @@ func newKVClient(storeType string, address string, timeout int) (kvstore.Client,
 	case "consul":
 		return kvstore.NewConsulClient(address, timeout)
 	case "etcd":
-		return kvstore.NewEtcdClient(address, timeout)
+		return kvstore.NewEtcdClient(address, timeout, log.WarnLevel)
 	}
 	return nil, errors.New("unsupported-kv-store")
 }

pkg/techprofile/tech_profile.go
@@ -275,7 +275,7 @@ func newKVClient(storeType string, address string, timeout int) (kvstore.Client,
 	case "consul":
 		return kvstore.NewConsulClient(address, timeout)
 	case "etcd":
-		return kvstore.NewEtcdClient(address, timeout)
+		return kvstore.NewEtcdClient(address, timeout, log.WarnLevel)
 	}
 	return nil, errors.New("unsupported-kv-store")
 }