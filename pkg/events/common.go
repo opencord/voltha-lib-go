856869c5108bbf06d8126f0241c5f02dffa97d33
Mahir Gunyel <mahir.gunyel@netsia.com>
2021-05-13 08:58:16 -0700

    VOL-4079 Publishing device state changes on bus
    Change-Id: If32cf5b81fcab1b0095931be7f7b474141d4886b

--- pkg/events/common.go
+++ pkg/events/common.go
@@ -19,6 +19,35 @@ import (
 	"github.com/opencord/voltha-lib-go/v4/pkg/log"
 )
 
+const (
+	// ContextAdminState is for the admin state of the Device in the context of the event
+	ContextAdminState = "admin-state"
+	// ContextConnectState is for the connect state of the Device in the context of the event
+	ContextConnectState = "connect-state"
+	// ContextOperState is for the operational state of the Device in the context of the event
+	ContextOperState = "oper-state"
+	// ContextPrevdminState is for the previous admin state of the Device in the context of the event
+	ContextPrevAdminState = "prev-admin-state"
+	// ContextPrevConnectState is for the previous connect state of the Device in the context of the event
+	ContextPrevConnectState = "prev-connect-state"
+	// ContextPrevOperState is for the previous operational state of the Device in the context of the event
+	ContextPrevOperState = "prev-oper-state"
+	// ContextDeviceID is for the previous operational state of the Device in the context of the event
+	ContextDeviceID = "id"
+	// ContextParentID is for the parent id in the context of the event
+	ContextParentID = "parent-id"
+	// ContextSerialNumber is for the serial number of the Device in the context of the event
+	ContextSerialNumber = "serial-number"
+	// ContextIsRoot is for the root flag of Device in the context of the event
+	ContextIsRoot = "is-root"
+	// ContextParentPort is for the parent interface id of child in the context of the event
+	ContextParentPort = "parent-port"
+)
+
+const (
+	DeviceStateChangeEvent = "DEVICE_STATE_CHANGE"
+)
+
 var logger log.CLogger
 
 func init() {
